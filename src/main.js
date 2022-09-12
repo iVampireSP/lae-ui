41768a992b2813d17f9e0cea0617863b2d81dc2a
iVampireSP.com <im@ivampiresp.com>
2022-09-12 21:27:21 +0800

    害

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './assets/css/bootstrap-dark.min.css';
 import './style.css';
