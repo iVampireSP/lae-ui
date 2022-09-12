3751b459bca940c752f56b6497b61bd05587706f
iVampireSP.com <im@ivampiresp.com>
2022-09-13 02:33:23 +0800

    改进 登录提示框

--- src/components/Modals.vue
+++ src/components/Modals.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="modal fade"
+    class="modal modal-dialog-centered fade"
     id="tokenExpiredModal"
     data-bs-backdrop="static"
     data-bs-keyboard="false"
@@ -13,12 +13,15 @@
         <div class="modal-header">
           <h5 class="modal-title" id="tokenExpiredLabel">密钥已失效</h5>
         </div>
-        <div class="modal-body">您必须重新登录才能继续使用 莱云 Web 客户端。</div>
+        <div class="modal-body">
+          您必须重新登录才能继续使用 莱云 Web 客户端。
+        </div>
         <div class="modal-footer">
           <button
             type="button"
             class="btn btn-secondary"
             data-bs-dismiss="modal"
+            @click="toLogin()"
           >
             重新登录
           </button>
@@ -27,3 +30,15 @@
     </div>
   </div>
 </template>
+
+<script setup>
+  import store from '../plugins/store'
+  import router from '../plugins/router'
+
+  function toLogin() {
+    store.commit('updateToken', {
+      token: null,
+    })
+    router.push({ name: 'login' })
+  }
+</script>
