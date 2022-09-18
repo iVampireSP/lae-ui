a07ecf5b96f872b6634f26075a6be935d239f428
iVampireSP.com <im@ivampiresp.com>
2022-09-18 16:25:37 +0800

    改进 显示

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,38 +2,43 @@
   <div class="mb-2">
     <h4>欢迎使用 莱云 Beta</h4>
 
-    <!-- <span v-if="announcement.attributes">
-      {{ announcement.attributes.title }}
+    <!-- <span v-if="item.attributes">
+      {{ item.attributes.title }}
     </span>
 
-    <span v-if="announcement.attributes">
-      {{ new Date(announcement.attributes.createdAt).toLocaleString() }}
+    <span v-if="item.attributes">
+      {{ new Date(item.attributes.createdAt).toLocaleString() }}
     </span> -->
-    <h4 class="mt-3">公告</h4>
-    <div class="list-group mt-3" v-for="announcement in announcements">
-      <span v-if="announcement.attributes">
-        <a
-          class="list-group-item list-group-item-action shadow-sm rounded"
-          target="_blank"
-          :href="base_url + '/d/' + announcement.attributes.slug"
-        >
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1 text-success">
-              {{ announcement.attributes.title }}
-            </h5>
-            <small>
-              {{
-                new Date(announcement.attributes.lastPostedAt).toLocaleString()
-              }}
-            </small>
-          </div>
-          <!-- <p class="mb-1">
+
+    <div v-show="pinned">
+      <h4 class="mt-3">公告</h4>
+      <div class="list-group mt-3" v-for="item in pinned">
+        <span v-if="item.attributes">
+          <a
+            class="list-group-item list-group-item-action shadow-sm rounded"
+            target="_blank"
+            :href="base_url + '/d/' + item.attributes.slug"
+          >
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1 text-success">
+                {{ item.attributes.title }}
+              </h5>
+              <small>
+                {{
+                  new Date(
+                    item.attributes.lastPostedAt
+                  ).toLocaleString()
+                }}
+              </small>
+            </div>
+            <!-- <p class="mb-1">
             <span></span>
           </p> -->
-          <!-- <small class="text-muted">
+            <!-- <small class="text-muted">
         </small> -->
-        </a>
-      </span>
+          </a>
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -42,22 +47,13 @@
   import { ref } from 'vue'
   import http from '../api/http'
 
-  const announcements = ref([
-    {
-      attributes: {
-        title: '莱云 Beta',
-        slug: 'beta',
-      },
-    },
-  ])
+  const pinned = ref([])
   const base_url = ref('')
 
-  http.get('forum/announcements').then((res) => {
+  http.get('forum/pinned').then((res) => {
     // foreach res
-    announcements.value = res.data
+    pinned.value = res.data
 
-    console.log(announcements.value)
-
-    base_url.value = announcements.value.base_url
+    base_url.value = pinned.value.base_url
   })
 </script>
