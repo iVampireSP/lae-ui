0554f3ddbc60b3d59256e5e043874b29856fb7c9
iVampireSP.com <im@ivampiresp.com>
2022-09-23 19:33:34 +0800

    修复 颜色

--- src/views/user/Hosts.vue
+++ src/views/user/Hosts.vue
@@ -49,7 +49,7 @@
                 >
               </span>
               <span v-else-if="host.status == 'pending'">
-                <span class="text-success"
+                <span class="text-warning"
                   ><i class="bi bi-clock-history"></i> &nbsp;等待中</span
                 >
               </span>
