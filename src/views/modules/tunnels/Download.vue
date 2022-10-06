5b2e3b8fde8b938e05c0fa275e913cfba92cb05d
iVampireSP.com <im@ivampiresp.com>
2022-10-06 19:05:40 +0800

    update

--- src/views/modules/tunnels/Download.vue
+++ src/views/modules/tunnels/Download.vue
@@ -8,28 +8,28 @@
 <table class="table table-responsive table-hover table-align">
       <thead>
         <tr>
-          <th>系统类型</th>
-          <th>系统架构</th>
+          <th>类型</th>
+          <th>架构</th>
           <th>下载地址</th>
-          <th>下载文件</th>
+          <th>下载</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>Windows 图形启动器</td>
-          <td>AMD64</td>
+          <td>amd64</td>
           <td><code>http://cdn.114514.space/Download/Files/2.0.0/MEFrp_GUI_V1.0.1_Released.zip</code></td>
           <td><a href="http://cdn.114514.space/Download/Files/2.0.0/MEFrp_GUI_V1.0.1_Released.zip">点击下载</a></td>
         </tr>
         <tr>
-          <td>Windows启动器核心</td>
-          <td>AMD64</td>
+          <td>Windows 启动器核心</td>
+          <td>amd64</td>
           <td><code>https://cdn.114514.space/Download/Files/2.0.0/frp_0.44.0_windows_amd64.zip</code></td>
           <td><a href="https://cdn.114514.space/Download/Files/2.0.0/frp_0.44.0_windows_amd64.zip">点击下载</a></td>
         </tr>
         <tr>
-          <td>Linux启动器核心</td>
-          <td>AMD64</td>
+          <td>Linux 启动器核心</td>
+          <td>amd64</td>
           <td><code>https://cdn.114514.space/Download/Files/2.0.0/frp_0.44.0_linux_amd64.tar.gz</code></td>
           <td><a href="https://cdn.114514.space/Download/Files/2.0.0/frp_0.44.0_linux_amd64.tar.gz">点击下载</a></td>
         </tr>
