62c04f90e38ce5909936c7d853198085e0ca7503
iVampireSP.com <im@ivampiresp.com>
2022-10-19 16:22:17 +0800

    加载动画

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="mb-2">
-    <h4>欢迎使用 莱云 Beta</h4>
+  <div
+    v-show="!show"
+    style="width: 100%; height: 500px"
+    id="chart-container"
+  ></div>
+
+  <div class="mb-2" v-show="show">
+    <h4>欢迎使用 莱云</h4>
 
     <!-- <span v-if="item.attributes">
       {{ item.attributes.title }}
@@ -40,16 +46,76 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  import { ref, onMounted } from 'vue'
   import http from '../api/http'
+
+  import * as echarts from 'echarts/core'
+  import { GraphicComponent } from 'echarts/components'
+  import { SVGRenderer } from 'echarts/renderers'
+
+  echarts.use([GraphicComponent, SVGRenderer])
 
   const pinned = ref([])
   const base_url = ref('')
+  const show = ref(false)
 
   http.get('forum/pinned').then((res) => {
-    // foreach res
     pinned.value = res.data
 
     base_url.value = pinned.value.base_url
+
+    show.value = true
+  })
+
+  onMounted(() => {
+    var chartDom = document.getElementById('chart-container')
+    var myChart = echarts.init(chartDom, null, {
+      renderer: 'svg',
+    })
+    var option
+
+    option = {
+      graphic: {
+        elements: [
+          {
+            type: 'text',
+            left: 'center',
+            top: 'center',
+            style: {
+              text: '莱云',
+              fontSize: 80,
+              fontWeight: 'bold',
+              lineDash: [0, 200],
+              lineDashOffset: 0,
+              fill: 'transparent',
+              stroke: '#000',
+              lineWidth: 1,
+            },
+            keyframeAnimation: {
+              duration: 1500,
+              loop: false,
+              keyframes: [
+                {
+                  percent: 0.7,
+                  style: {
+                    fill: 'transparent',
+                    lineDashOffset: 200,
+                    lineDash: [200, 0],
+                  },
+                },
+                {
+                  percent: 1,
+                  style: {
+                    fill: 'black',
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    }
+
+    option && myChart.setOption(option)
   })
 </script>
