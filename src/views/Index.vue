dd5d1f102230899993d6c0e310dbd7aba129a03a
iVampireSP.com <im@ivampiresp.com>
2022-10-20 12:35:28 +0800

    改进 首页加载动画

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -46,7 +46,7 @@
 </template>
 
 <script setup>
-  import { ref, onMounted } from 'vue'
+  import { ref, onMounted, onUnmounted } from 'vue'
   import http from '../api/http'
 
   import * as echarts from 'echarts/core'
@@ -64,16 +64,64 @@
 
     base_url.value = pinned.value.base_url
 
-    show.value = true
+    // show.value = true
   })
 
+  function randomInt() {
+    return Math.floor(Math.random() * 100)
+  }
+
+  let fontSize = 100
+  let color = 'black'
+
+  let sentences = [
+    'Make LAE great again!',
+    '莱云',
+    'LAECLOUD',
+    '正在点咖啡',
+    '正在拉花',
+    '正在制作卡布奇诺',
+    '正在制作拿铁',
+    'Mirror Edge 现在是莱云的一部分!',
+    '齉龘齉齾爩麤龗灪龖厵纞虋',
+    '锟斤拷烫烫烫屯屯屯'
+  ]
+  // pick
+  let sentence = sentences[Math.floor(Math.random() * sentences.length)]
+
+  function resizeFont() {
+    if (window.innerWidth < 768) {
+      fontSize = 40
+
+      if (sentence.length > 20) {
+        fontSize = 20
+      }
+
+      //   如果渲染后字体太大，就缩小
+      if (sentence.length * fontSize > window.innerWidth) {
+        fontSize = window.innerWidth / sentence.length
+      }
+    } else {
+      if (sentence.length > 20) {
+        fontSize = 50
+      }
+    }
+  }
+  // if dark mode
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    color = 'white'
+  }
+
   onMounted(() => {
+    resizeFont()
+
     var chartDom = document.getElementById('chart-container')
     var myChart = echarts.init(chartDom, null, {
       renderer: 'svg',
     })
     var option
 
+    let lineDash = randomInt() * 10
     option = {
       graphic: {
         elements: [
@@ -82,33 +130,33 @@
             left: 'center',
             top: 'center',
             style: {
-              text: '莱云',
-              fontSize: 80,
-              fontWeight: 'bold',
-              lineDash: [0, 200],
+              text: sentence,
+              fontSize: fontSize,
+              fontWeight: 300,
+              lineDash: [0, randomInt()],
               lineDashOffset: 0,
               fill: 'transparent',
-              stroke: '#000',
+              stroke: color,
               lineWidth: 1,
             },
             keyframeAnimation: {
-              duration: 1500,
+              duration: 1000,
               loop: false,
               keyframes: [
                 {
-                  percent: 0.7,
+                  percent: 0.5,
                   style: {
                     fill: 'transparent',
-                    lineDashOffset: 200,
-                    lineDash: [200, 0],
+                    lineDashOffset: lineDash,
+                    lineDash: [lineDash, 0],
                   },
                 },
                 {
                   percent: 1,
                   style: {
-                    fill: 'black',
+                    fill: color,
                   },
-                },
+                }
               ],
             },
           },
@@ -117,5 +165,19 @@
     }
 
     option && myChart.setOption(option)
+
+    // when resize, resize chart
+    window.addEventListener('resize', () => {
+      myChart.resize()
+
+      resizeFont()
+    })
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('resize', () => {
+      myChart.resize()
+      resizeFont()
+    })
   })
 </script>
