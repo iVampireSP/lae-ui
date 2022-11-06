8996f955f3fe4c81025c81586996a27558e52c33
iVampireSP.com <im@ivampiresp.com>
2022-11-06 23:53:29 +0800

    修改 服务器地址

--- src/config/api.js
+++ src/config/api.js
@@ -1,14 +1,14 @@
 const config = {
   dev: {
     api: 'http://www.lae.test/api/',
-    auth: 'http://www.auth_server.test',
+    auth: 'http://www.lae.test',
     ws_host: 'www.lae.test',
     ws_port: 6001,
     ws_auth_endpoint: 'http://www.lae.test/api/broadcasting/auth',
   },
   prod: {
     api: 'https://api.lae.yistars.net/api/',
-    auth: 'https://auth.laecloud.com',
+    auth: 'https://www.laecloud.com',
 
     ws_host: 'socket.lae.yistars.net',
     ws_port: 443,
