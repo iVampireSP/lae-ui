21f4d4a4fafa2296d1f88dc3ca059a1dff29b05f
iVampireSP.com <im@ivampiresp.com>
2022-11-30 16:10:32 +0800

    增加 元数据显示

--- src/views/Servers.vue
+++ src/views/Servers.vue
@@ -10,6 +10,7 @@
           <tr>
             <th>服务</th>
             <th>名称和状态</th>
+            <th>元数据</th>
           </tr>
         </thead>
         <tbody>
@@ -35,6 +36,15 @@
                 </span>
               </span>
               &nbsp; {{ server.name }}
+            </td>
+
+            <td>
+              <span v-if="server.meta.length !== 0">
+                <a @click="showMeta(server.meta)" class="cursor-pointer"
+                  >显示</a
+                >
+              </span>
+              <span v-else> 无 </span>
             </td>
           </tr>
         </tbody>
@@ -80,15 +90,68 @@
       </table>
     </div>
   </div>
+
+  <!-- model to display meta -->
+  <div
+    class="modal fade"
+    id="metaModal"
+    tabindex="-1"
+    aria-labelledby="metaModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="metaModalLabel">元数据</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div
+          class="modal-body overflow-auto p-0  rounded"
+        >
+          <table class="table table-striped table-hover">
+            <thead>
+              <tr>
+                <th>键</th>
+                <th>值</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(value, key) in meta" :key="key">
+                <td>{{ key }}</td>
+                <td>{{ value }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            关闭
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
   import http from '../api/http'
   import echo from '../plugins/echo'
   import { ref, onMounted, onUnmounted } from 'vue'
+  import { Modal } from 'bootstrap'
 
   const servers = ref([])
   const modules = ref([])
+
+  const meta = ref([])
 
   http.get('/servers').then((res) => {
     servers.value = res.data
@@ -97,6 +160,12 @@
   http.get('/modules').then((res) => {
     modules.value = res.data
   })
+
+  function showMeta(meta_data) {
+    meta.value = meta_data
+    const myModal = new Modal(document.getElementById('metaModal'))
+    myModal.show()
+  }
 
   onMounted(() => {
     echo.channel('servers').listen('.servers', (e) => {
