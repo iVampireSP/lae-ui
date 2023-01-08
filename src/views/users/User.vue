e6b02758f26b33cb1c02665d3c08f65dcfcb81dc
iVampireSP.com <im@ivampiresp.com>
2023-01-08 21:11:28 +0800

    添加文件

---
+++ src/views/users/User.vue
@@ -0,0 +1,27 @@
+<template>
+  <n-avatar :size="isMobile ? 64 : 256" :src="avatar" lazy round/>
+  <NH1>{{ user.name }} # {{ user.id }}</NH1>
+</template>
+
+<script setup>
+import {computed} from "vue";
+import userStore from "../../plugins/stores/user.js";
+import conf from "../../config/api.js";
+
+import {NAvatar, NH1} from 'naive-ui'
+
+
+import {useIsMobile} from "../../utils/composables.js";
+
+
+const user = computed(() => {
+  return userStore.state.user
+})
+
+const isMobile = useIsMobile()
+
+const avatar = computed(() => {
+  return conf.avatar + user.value.email_md5 + '?s=256';
+})
+
+</script>
