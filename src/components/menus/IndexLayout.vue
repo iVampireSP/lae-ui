74aca826b5c6472b51e417d1d97e3b1b5e1ed4d9
iVampireSP.com <im@ivampiresp.com>
2023-01-10 00:21:29 +0800

    改进 菜单图标

--- src/components/menus/IndexLayout.vue
+++ src/components/menus/IndexLayout.vue
@@ -8,12 +8,15 @@
 
 import {addMenuOptions, removeAllMenuOptionsThen} from "../../config/menuOptions.js";
 
+import {HomeOutline, PinOutline} from "@vicons/ionicons5";
+import {MessageOutlined} from "@vicons/material"
+
 removeAllMenuOptionsThen('left', () => {
 
 // 注册菜单
-  addMenuOptions('left', 'index', 'Index',)
-  addMenuOptions('left', 'announcements', '公告')
-  addMenuOptions('left', 'pinned', '固钉')
+  addMenuOptions('left', 'index', '首页', HomeOutline)
+  addMenuOptions('left', 'announcements', '公告', MessageOutlined)
+  addMenuOptions('left', 'pinned', '固钉', PinOutline)
 })
 
 
