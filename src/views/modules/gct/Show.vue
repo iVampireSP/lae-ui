053b423d10ad910d2e61b740f2667599439c70c6
iVampireSP <im@ivampiresp.com>
2023-01-16 20:18:05 +0800

    改进 电源操作处理 终端 以及 服务器配置调整

--- src/views/modules/gct/Show.vue
+++ src/views/modules/gct/Show.vue
@@ -5,94 +5,144 @@
         {{ gct.name }}
       </n-gradient-text>
     </n-h1>
-    <n-grid cols="1 s:4" responsive="screen" x-gap="12" class="text-center">
-      <n-gi>
-        <n-h3>电源操作</n-h3>
-        <n-button-group>
-          <n-button strong secondary type="info" @click="powerAction('start')">
-            开机
-          </n-button>
-          <n-button strong secondary type="error" @click="powerAction('stop')" v-if="stopping === false">
-            停止
-          </n-button>
-          <n-button strong secondary type="error" @click="powerAction('kill')" v-else>
-            强制停止
-          </n-button>
-          <n-button strong secondary type="warning" @click="powerAction('restart')">
-            重启
-          </n-button>
-        </n-button-group>
+    <n-grid cols="1 xl:7 " responsive="screen">
+      <n-gi span="5 1">
+        <n-h3 class="!mb-0">控制面板</n-h3>
+        <n-a target="_blank" :href=" api.modules.gct.panel  + '/server/' + gct.server.identifier">
+          控制面板
+        </n-a>
+
+        <div class="mt-5 mb-5">
+          <n-h3 class="!mb-0">电源控制</n-h3>
+
+          <n-button-group v-if="ct">
+            <n-button strong secondary type="info" @click="ct.setState('start')"
+                      :disabled="ct.state.current === 'running' || ct.state.current === 'starting'">
+              开机
+            </n-button>
+            <n-button strong secondary type="error" @click="ct.setState('stop')"
+                      v-if="ct.state.current === 'running' || ct.state.current === 'starting' ">
+              停止
+            </n-button>
+            <n-button strong secondary type="error" @click="ct.setState('kill')"
+                      v-else-if="ct.state.current === 'stopping'">
+              强制停止
+            </n-button>
+            <n-button strong secondary type="error" v-else disabled>
+              停止
+            </n-button>
+            <n-button strong secondary type="warning" @click="ct.setState('restart')">
+              重启
+            </n-button>
+          </n-button-group>
+
+          <div v-else>
+            切换到“控制台”选项卡以查看电源控制。
+          </div>
+        </div>
+
+        <n-h3 class="!mb-0 !mt-0">使用率</n-h3>
       </n-gi>
-      <n-gi>
-        <n-h3>性能调整</n-h3>
-        <n-spin :show="modifying">
-          <n-form ref="form" :model="gct">
-            <!-- :rules="rules" -->
-            <n-form-item label="CPU 限制">
-              <n-slider v-model:value="gct.cpu_limit" :default-value="gct.cpu_limit" :step="50" :max="800" :min="100"
-                        :show-tooltip="gct.cpu_limit > 400" :format-tooltip="formatCpuLimitTooltip"/>
-            </n-form-item>
-            <n-form-item label="内存">
-              <n-slider v-model:value="gct.memory" :default-value="gct.memory" :step="512" :max="10240" :min="512"
-                        :show-tooltip="gct.memory > 2048" :format-tooltip="formatTooltip"/>
-            </n-form-item>
-            <n-form-item label="存储大小">
-              <n-slider v-model:value="gct.disk" :default-value="gct.disk" :step="512" :max="10240" :min="512"
-                        :show-tooltip="gct.disk > 4096" :format-tooltip="formatDiskTooltip"/>
-            </n-form-item>
-            <n-button type="primary" ghost attr-type="submit" @click="update()">保存</n-button>
-          </n-form>
-        </n-spin>
-      </n-gi>
-      <n-gi>
-        <n-h3>高级设置</n-h3>
-        <n-spin :show="modifying">
-          <n-form ref="form" :model="gct">
-            <!-- :rules="rules" -->
-            <n-form-item label="备份数量">
-              <n-slider v-model:value="gct.backups" :default-value="gct.backups" :step="1" :max="8" :min="1"
-                        :show-tooltip="gct.backups > 3" :format-tooltip="formatCommonTooltip"/>
-            </n-form-item>
-            <n-form-item label="端口数量">
-              <n-slider v-model:value="gct.allocations" :default-value="gct.allocations" :step="1" :max="10" :min="1"
-                        :show-tooltip="gct.allocations > 3" :format-tooltip="formatCommonTooltip"/>
-            </n-form-item>
-            <n-form-item label="数据库数量">
-              <n-slider v-model:value="gct.databases" :default-value="gct.databases" :step="1" :max="5" :min="0"
-                        :show-tooltip="gct.databases > 0" :format-tooltip="formatCommonTooltip"/>
-            </n-form-item>
-            <n-button type="primary" ghost attr-type="submit" @click="update()">保存</n-button>
-          </n-form>
-        </n-spin>
-      </n-gi>
-      <n-gi>
-        <n-h3>重设模板</n-h3>
+      <n-gi span="6">
+        <n-tabs type="line" animated>
+          <n-tab-pane name="console" tab="控制台">
+            <Terminal class="text-left" :height="90" :gct_id="gct.id" v-if="gct.id" ref="ct"/>
+          </n-tab-pane>
+
+          <n-tab-pane name="advanced" tab="调整">
+
+            <n-spin :show="modifying">
+              <n-grid cols="1 s:2" responsive="screen" x-gap="12">
+                <n-gi>
+                  <n-form-item label="容器名称" path="name">
+                    <n-input v-model:value="gct.name"/>
+                  </n-form-item>
+
+                  <n-form-item label="要开的服务器" path="egg_id">
+                    <n-select v-model:value="gct.egg_id" :options="eggs"/>
+
+                  </n-form-item>
+
+                  <n-form-item label="端口数量">
+                    <n-slider v-model:value="gct.allocations" :default-value="1" :step="1" :max="10" :min="1"
+                              :show-tooltip="gct.allocations > 3" :format-tooltip="formatCommonTooltip"/>
+                  </n-form-item>
+
+                  <n-form-item label="数据库数量">
+                    <n-slider v-model:value="gct.databases" :default-value="gct.databases" :step="1" :max="5" :min="0"
+                              :show-tooltip="gct.databases > 1" :format-tooltip="formatCommonTooltip"/>
+                  </n-form-item>
+
+                </n-gi>
+                <n-gi>
+                  <n-form-item label="内存">
+                    <n-slider v-model:value="gct.memory" :default-value="1024" :step="512" :max="10240" :min="512"
+                              :show-tooltip="gct.memory > 2048" :format-tooltip="formatTooltip"/>
+                  </n-form-item>
+                  <n-form-item label="CPU 限制">
+                    <n-slider v-model:value="gct.cpu_limit" :default-value="100" :step="50" :max="800" :min="100"
+                              :show-tooltip="gct.cpu_limit > 400" :format-tooltip="formatCpuLimitTooltip"/>
+                  </n-form-item>
+                  <n-form-item label="存储大小">
+                    <n-slider v-model:value="gct.disk" :default-value="512" :step="512" :max="10240" :min="512"
+                              :show-tooltip="gct.disk > 4096" :format-tooltip="formatDiskTooltip"/>
+                  </n-form-item>
+                  <n-form-item label="备份数量">
+                    <n-slider v-model:value="gct.backups" :default-value="1" :step="1" :max="8" :min="1"
+                              :show-tooltip="gct.backups > 3" :format-tooltip="formatCommonTooltip"/>
+                  </n-form-item>
+                </n-gi>
+              </n-grid>
+
+              <div class="text-right mt-3 mr-1 mb-1">
+                <n-button type="primary" ghost @click="update()">保存</n-button>
+              </div>
+            </n-spin>
+          </n-tab-pane>
+
+          <n-tab-pane name="boot_template" tab="启动模板" class="mb-2">
+            <n-select v-model:value="gct.egg_id" :options="eggs"/>
+
+            <div class="text-right mt-3">
+              <n-button type="primary" ghost @click="update({
+                egg_id: gct.egg_id
+              })">保存
+              </n-button>
+            </div>
+          </n-tab-pane>
+        </n-tabs>
+
       </n-gi>
     </n-grid>
-    <div class="light-green mt-8"/>
   </div>
 </template>
 
 <script setup>
 import {ref} from 'vue'
 import {
+  NA,
   NButton,
   NButtonGroup,
-  NForm,
   NFormItem,
   NGi,
   NGradientText,
   NGrid,
   NH1,
   NH3,
+  NInput,
+  NSelect,
   NSlider,
   NSpin,
+  NTabPane,
+  NTabs,
   useDialog,
   useMessage
 } from 'naive-ui'
+import Terminal from './components/Terminal.vue'
 
 import http from '../../../plugins/http'
 import {useRoute} from "vue-router";
+import api from '../../../config/api'
 
 const router = useRoute()
 
@@ -113,8 +163,13 @@ const gct = ref({
   cpu_limit: 100,
   allocations: 1,
   backups: 1,
-  databases: 1,
+  databases: 0,
+  server: {
+    identifier: ''
+  }
 })
+
+const ct = ref(null)
 
 const formatTooltip = (value) => `${value / 1024} GB`
 const formatCpuLimitTooltip = (value) => `相当于 ${value / 100} 核`
@@ -128,42 +183,11 @@ http.get('/modules/gct/hosts/' + router.params.id).then((res) => {
 
 
 function update() {
-}
-
-function powerAction(type) {
-  if (type === "kill") {
-    dialog.warning({
-      title: "警告",
-      content: "你确定要强制停止服务器吗, 这样做可能会丢失数据!",
-      positiveText: "确定",
-      negativeText: "取消",
-      onPositiveClick: () => {
-        http.post('/modules/gct/hosts/' + router.params.id + '/server/power?signal=kill').then((res) => {
-          message.success("正在强制停止服务器")
-          stopping.value = false
-        })
-      }
-    })
-    return
-  }
-  // type: restart start stop kill
-  http.post('/modules/gct/hosts/' + router.params.id + '/server/power?signal=' + type).then((res) => {
-    if (type === "restart") {
-      message.success("正在重启服务器")
-    } else if (type === "start") {
-      message.success("正在启动服务器")
-    } else if (type === "stop") {
-      message.success("正在停止服务器")
-      stopping.value = true
-    }
+  modifying.value = true
+  http.patch('/modules/gct/hosts/' + router.params.id, gct.value).finally(() => {
+    modifying.value = false
   })
 }
 
-</script>
 
-<style scoped>
-.light-green {
-  height: 108px;
-  background-color: rgba(0, 128, 0, 0.12);
-}
-</style>
+</script>
