2d8d8467c9a18f01acbd780b6312eb8e8896c6a2
iVampireSP <im@ivampiresp.com>
2023-01-17 16:49:07 +0800

    添加 运营

--- src/views/modules/tunnels/Base.vue
+++ src/views/modules/tunnels/Base.vue
@@ -1,5 +1,12 @@
 <template>
   <router-view/>
+
+    <n-text depth="3" tag="div" class="text-center mt-5">
+      <span>Aehxy 创建 / 策划 / 运营</span>
+      <br/>
+      <span>莱云 所有</span>
+    </n-text>
+
 </template>
 
 <script setup>
@@ -10,7 +17,16 @@ import {
   removeAllMenuOptionsThen
 } from "../../../config/menuOptions.js";
 
-import {AddOutline, ClipboardOutline, CloudDownloadOutline, ListOutline, ServerOutline} from "@vicons/ionicons5";
+import {NText} from "naive-ui";
+
+import {
+  AddOutline,
+  CashOutline,
+  ClipboardOutline,
+  CloudDownloadOutline,
+  ListOutline,
+  ServerOutline,
+} from "@vicons/ionicons5";
 
 import {ref} from 'vue'
 
@@ -29,8 +45,11 @@ function reRegisterMenu() {
     addMenuOptions('left', 'modules.tunnels.index', '所有隧道', ListOutline)
     addMenuOptions('left', 'modules.tunnels.create', '新建隧道', AddOutline)
     addMenuOptions('left', 'modules.tunnels.concat', '配置文件', ClipboardOutline)
-    addMenuOptions('left', 'modules.tunnels.status', '服务器', ServerOutline)
+    addMenuOptions('left', 'modules.tunnels.status', '服务状态', ServerOutline)
+    addMenuOptions('left', 'modules.tunnels.sponsor', '赞助我们', CashOutline)
     addMenuOptions('left', 'modules.tunnels.downloads', '客户端下载', CloudDownloadOutline)
+
+
 
     if (tunnels.value.length > 0) {
       addMenuDivider('left')
