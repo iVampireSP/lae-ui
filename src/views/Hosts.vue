e3b1d9f73a59554fc4c244abd12eb9fe35bef345
iVampireSP <im@ivampiresp.com>
2023-01-18 00:48:41 +0800

    基础铺垫

--- src/views/Hosts.vue
+++ src/views/Hosts.vue
@@ -1,8 +1,34 @@
 <template>
-  <n-h1>仍在开发</n-h1>
+  <div>
+    <n-h1 prefix="bar">
+      <n-text type="primary">
+        主机列表
+      </n-text>
+    </n-h1>
+    <n-p v-if="user.user_group_id !== null">
+      此账号拥有 {{ user.user_group.discount }}% 的折扣。
+      <span v-if="user.user_group.exempt"> 并且豁免计费。 </span>
+      <span style="color: #d03050;">价格均为使用折扣后的价格。</span>
+    </n-p>
+  </div>
 </template>
 
 <script setup>
-import {NH1} from 'naive-ui'</script>
+import {
+  NH1,
+  NText,
+  NP
+} from 'naive-ui'
+import userStore from '../plugins/stores/user.js'
+import {computed} from "vue";
+
+const user = computed(() => {
+  return userStore.state.user
+})
+
+const columns = []
+const data = []
+
+</script>
 
 <style scoped></style>
