305ae62f555629d560922e71e6a6a30fc31aef7e
iVampireSP.com <im@ivampiresp.com>
2023-01-21 22:02:52 +0800

    移除 开发提示

--- src/views/modules/gct/Index.vue
+++ src/views/modules/gct/Index.vue
@@ -6,19 +6,12 @@
       </n-text>
     </n-h1>
 
-    <n-alert class="mb-3" closable title="注意" type="warning" v-for="i in 3">
-      整个 游戏容器 的页面我们仍在开发，你所看到的在这里有关 游戏容器 的所有内容都是未完成品，不代表最终品质。<br/>
-      如果您想使用游戏容器，工单等服务，请
-      <n-a href="https://web.laecloud.com">点击这里回到旧版仪表盘</n-a>
-      。
-    </n-alert>
-    
-    <ContainersList :containers="containers" :next="go" />
+    <ContainersList :containers="containers" :next="go"/>
   </div>
 </template>
 
 <script setup>
-import {NA, NAlert, NH1, NText} from 'naive-ui'
+import {NH1, NText} from 'naive-ui'
 
 import {computed} from 'vue'
 
