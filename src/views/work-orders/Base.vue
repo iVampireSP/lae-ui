a9671c55c9c49ab970eef243073619f9dcc0138b
iVampireSP.com <im@ivampiresp.com>
2023-01-31 19:42:00 +0800

    工单 框架

---
+++ src/views/work-orders/Base.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+<script setup>
+import {addMenuOptions, removeAllMenuOptionsThen} from "../../config/menuOptions.js"
+import {MessageOutlined} from "@vicons/material"
+import {HomeOutline, PinOutline} from "@vicons/ionicons5"
+
+removeAllMenuOptionsThen('left', () => {
+
+// 注册菜单
+  addMenuOptions('left', 'index', '首页', HomeOutline)
+  addMenuOptions('left', 'pinned', '固钉', PinOutline)
+  addMenuOptions('left', 'announcements', '公告', MessageOutlined)
+})
+</script>
--- src/views/work-orders/Index.vue
+++ src/views/work-orders/Index.vue
@@ -1,27 +1,17 @@
 <template>
-  <n-h1 prefix="bar">
-    <n-text type="primary">
-      工单
-    </n-text>
-  </n-h1>
-  <n-alert title="抱歉" type="warning">
-    工单页面正在开发中，因此暂时无法使用，敬请期待。
-  </n-alert>
+  <Base>
+    <n-h1 prefix="bar">
+      <n-text type="info">
+        工单
+      </n-text>
+    </n-h1>
+    <n-alert title="抱歉" type="warning">
+      工单页面正在开发中，因此暂时无法使用，敬请期待。
+    </n-alert>
+  </Base>
 </template>
 
-<script setup>
-import {NAlert, NH1, NText} from 'naive-ui'
-import {addMenuOptions, removeAllMenuOptionsThen} from "../../config/menuOptions.js"
-import {MessageOutlined} from "@vicons/material"
-import {HomeOutline, PinOutline} from "@vicons/ionicons5"
+<script setup lang="ts">
+import Base from "./Base.vue"
 
-removeAllMenuOptionsThen('left', () => {
-
-// 注册菜单
-  addMenuOptions('left', 'index', '首页', HomeOutline)
-  addMenuOptions('left', 'pinned', '固钉', PinOutline)
-  addMenuOptions('left', 'announcements', '公告', MessageOutlined)
-})
 </script>
-
-<style scoped></style>
