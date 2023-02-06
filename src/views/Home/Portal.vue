a1e90f10cd726df4a7df78fbbe1f1d06d9a3f3f7
iVampireSP.com <im@ivampiresp.com>
2023-02-06 23:52:47 +0800

    增加 链接

--- src/views/Home/Portal.vue
+++ src/views/Home/Portal.vue
@@ -20,10 +20,18 @@
     </div>
   </div>
 
+  <div class="absolute bottom-3 left-0 right-0">
+    <n-p class="text-center">
+      <n-text depth="3">
+        <n-a target="_blank" href="https://muhan.ltd">木韩网络</n-a> 提供服务
+      </n-text>
+    </n-p>
+  </div>
+
 </template>
 
 <script setup>
-import {NH1} from 'naive-ui'
+import {NH1, NP, NText, NA} from 'naive-ui'
 import LoginButton from "../../components/LoginButton.vue";
 
 //
