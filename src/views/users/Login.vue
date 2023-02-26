8a74e3a79d2752d6a94d1c88659cea26962add7e
iVampireSP.com <im@ivampiresp.com>
2023-02-26 22:03:33 +0800

    改进 登录

--- src/views/users/Login.vue
+++ src/views/users/Login.vue
@@ -2,68 +2,63 @@
   <div class="justify-center items-center flex" style="height: 60vh">
     <div>
       <div v-if="state === 'redirect'">
-        <Lottie :height="256"
-                :style="style"
-                name="Wave"
-        />
+        <Lottie :height="256" :style="style" name="Wave" />
 
         <div class="text-center">
           <n-h1>欢迎！要一起加入莱云吗？</n-h1>
 
-          <n-button :loading="loading" class="text-center" type="info" @click="toLogin">
+          <n-button
+            :loading="loading"
+            class="text-center"
+            type="info"
+            @click="toLogin"
+          >
             现在登录！
           </n-button>
         </div>
-
       </div>
       <div v-else-if="state === 'logging'">
         <div class="text-center">
-          <n-h2>正在等待验证</n-h2>
+          <span v-if="url">
+            <n-h2>正在等待验证</n-h2>
 
-          <span>次数: {{ times }} / 10</span>
+            <span>次数: {{ times }} / 10</span>
+            <br />
 
-          <br/>
-          <span>请
-            <n-a :href="url" target="_blank" class="underline underline-offset-4">点击打开 URL 并授权</n-a>
+            请
+            <n-a
+              :href="url"
+              target="_blank"
+              class="underline underline-offset-4"
+              >点击打开 URL 并授权</n-a
+            >
             。
           </span>
-        </div>
-
-      </div>
-      <div v-else-if="state === 'error'">
-        <Lottie :height="256"
-                :style="style"
-                name="Ghost"
-        />
-
-        <div class="text-center">
-          <n-h1 class="mt-5">
-            <n-text class="text-center" type="error">
-              无法完成登录。
-            </n-text>
-          </n-h1>
-
-          <n-button type="error" @click="toLogin">
-            重新登录
-          </n-button>
+          <span v-else>
+            <n-h2>正在加载...</n-h2>
+          </span>
         </div>
       </div>
       <div v-else-if="state === 'error'">
-        <Lottie :height="256"
-                :style="style"
-                name="Ghost"
-        />
+        <Lottie :height="256" :style="style" name="Ghost" />
 
         <div class="text-center">
           <n-h1 class="mt-5">
-            <n-text class="text-center" type="error">
-              无法完成登录。
-            </n-text>
+            <n-text class="text-center" type="error"> 无法完成登录。 </n-text>
           </n-h1>
 
-          <n-button type="error" @click="toLogin">
-            重新登录
-          </n-button>
+          <n-button type="error" @click="toLogin"> 重新登录 </n-button>
+        </div>
+      </div>
+      <div v-else-if="state === 'error'">
+        <Lottie :height="256" :style="style" name="Ghost" />
+
+        <div class="text-center">
+          <n-h1 class="mt-5">
+            <n-text class="text-center" type="error"> 无法完成登录。 </n-text>
+          </n-h1>
+
+          <n-button type="error" @click="toLogin"> 重新登录 </n-button>
         </div>
       </div>
 
@@ -81,52 +76,49 @@
 
       <p v-if="state === 'redirect'" class="text-center mt-5">
         莱云 iVampireSP.com 作品
-        <br/>
+        <br />
         <span>莱云的 Logo 动画由 Fofray 制作。</span>
-        <br/>
+        <br />
         <a href="https://beian.miit.gov.cn/">苏ICP备2022042268号-1</a>
       </p>
-
     </div>
   </div>
 </template>
 
 <script setup>
-import {onMounted, ref} from 'vue'
-import {NA, NButton, NH1, NH2, NH3, NText} from 'naive-ui'
-import http from '../../plugins/http'
-import api from '../../config/api'
-import user from "../../plugins/stores/user.js";
-import Lottie from "../../components/Lottie.vue";
-import axios from "axios";
+  import { onMounted, ref } from 'vue'
+  import { NA, NButton, NH1, NH2, NH3, NText } from 'naive-ui'
+  import http from '../../plugins/http'
+  import api from '../../config/api'
+  import user from '../../plugins/stores/user.js'
+  import Lottie from '../../components/Lottie.vue'
+  import axios from 'axios'
 
+  const style = {
+    textAlign: 'center',
+    marginTop: '20px',
+    marginBottom: '20px',
+  }
 
-const style = {
-  textAlign: 'center',
-  marginTop: '20px',
-  marginBottom: '20px'
-}
+  const origin = api.auth
 
-const origin = api.auth
+  console.log('auth server: ' + origin)
 
-console.log('auth server: ' + origin)
+  const token = ref('')
+  const state = ref('redirect')
 
-const token = ref('')
-const state = ref('redirect')
+  const auth_request = ref({})
 
-const auth_request = ref({})
+  const loading = ref(false)
 
-const loading = ref(false)
+  const url = ref('')
 
-const url = ref('')
+  const connect = () => {
+    loading.value = true
 
-const connect = () => {
+    user.commit('updateToken', token.value)
 
-  loading.value = true
-
-  user.commit('updateToken', token.value)
-
-  http
+    http
       .get('/user')
       .then((res) => {
         setTimeout(() => {
@@ -143,79 +135,78 @@ const connect = () => {
         user.commit('updateUser', {})
 
         loading.value = true
-
       })
-
-}
-
-let loginInter = null
-
-const times = ref(0)
-
-function toLogin() {
-  if (state.value === 'logging') {
-    return
   }
 
-  state.value = 'logging'
-  times.value = 0
+  let loginInter = null
 
-  loading.value = true
+  const times = ref(0)
 
+  function toLogin() {
+    if (state.value === 'logging') {
+      return
+    }
 
-  axios.post(api.auth + '/public/auth_request', {
-    description: '莱云仪表盘',
-    require_token: true,
-    abilities: ['*']
-  }).then(res => {
+    state.value = 'logging'
+    times.value = 0
+
     loading.value = true
 
+    axios
+      .post(api.auth + '/public/auth_request', {
+        description: '莱云仪表盘',
+        require_token: true,
+        abilities: ['*'],
+      })
+      .then((res) => {
+        loading.value = true
 
-    auth_request.value = res.data
-
-    url.value = res.data.url
-
-
-    loginInter = setInterval(() => {
-      axios.get(api.auth + '/public/auth_request/' + res.data.meta.token).then(res => {
         auth_request.value = res.data
 
-        if (res.data.user) {
-          token.value = res.data.token
-          state.value = 'confirm'
+        url.value = res.data.url
 
-          loginInter && clearInterval(loginInter)
+        loginInter = setInterval(() => {
+          axios
+            .get(api.auth + '/public/auth_request/' + res.data.meta.token)
+            .then((res) => {
+              auth_request.value = res.data
 
-          setTimeout(() => {
-            connect()
-          }, 500)
-        }
+              if (res.data.user) {
+                token.value = res.data.token
+                state.value = 'confirm'
+
+                loginInter && clearInterval(loginInter)
+
+                setTimeout(() => {
+                  connect()
+                }, 500)
+              }
+            })
+
+          if (times.value === 10) {
+            clearInterval(loginInter)
+            state.value = 'error'
+          }
+
+          times.value++
+        }, 3000)
       })
-
-      if (times.value === 10) {
-        clearInterval(loginInter)
+      .catch(() => {
         state.value = 'error'
-      }
+        loading.value = false
+      })
+      .finally(() => {
+        loading.value = false
+      })
+  }
 
-      times.value++
-    }, 3000)
-  }).catch(() => {
-    state.value = 'error'
-    loading.value = false
-
-  }).finally(() => {
-    loading.value = false
+  onMounted(() => {
+    user.commit('updateToken', '')
+    user.commit('updateUser', {})
   })
 
-}
-
-onMounted(() => {
-  user.commit('updateToken', '')
-  user.commit('updateUser', {})
-})
-
-const logo = ref('Logo-dark')
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  logo.value = 'Logo-white'
-}
+  const logo = ref('Logo-dark')
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    logo.value = 'Logo-white'
+  }
 </script>
