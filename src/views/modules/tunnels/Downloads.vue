0de9cc1a2541a65a609b28c9d3024b02598274bd
iVampireSP.com <im@ivampiresp.com>
2023-02-27 20:56:56 +0800

    修改 下载地址

--- src/views/modules/tunnels/Downloads.vue
+++ src/views/modules/tunnels/Downloads.vue
@@ -4,16 +4,14 @@
       <n-text type="primary"> 客户端下载</n-text>
     </n-h1>
 
-    <n-alert type="warning">
-      下载维护，请加 QQ 群 769779712。
-    </n-alert>
+    <n-alert type="info"> 欢迎加入 莱云咖啡厅, QQ 群号码: 769779712。 </n-alert>
 
-    <br/>
+    <br />
     <n-data-table
-        :columns="headers"
-        :data="items"
-        :filterOptions="filter_options"
-        :render-cell="render"
+      :columns="headers"
+      :data="items"
+      :filterOptions="filter_options"
+      :render-cell="render"
     />
     <!-- TODO: 完成表格筛选 -->
     <!-- @update:filters="archFilter" -->
@@ -39,79 +37,91 @@
 </template>
 
 <script setup>
-import {NA, NAlert, NDataTable, NH1, NText} from 'naive-ui'
-import {h, ref} from 'vue'
+  import { NA, NAlert, NDataTable, NH1, NText } from 'naive-ui'
+  import { h, ref } from 'vue'
 
-const headers = [
-  {
-    title: '名称',
-    key: 'name',
-  },
-  {
-    title: '架构',
-    key: 'arch',
-    filter() {
+  const headers = [
+    {
+      title: '名称',
+      key: 'name',
     },
-  },
-  {
-    title: '下载',
-    key: 'url',
-  },
-]
+    {
+      title: '架构',
+      key: 'arch',
+      filter() {},
+    },
+    {
+      title: '下载',
+      key: 'url',
+    },
+  ]
 
-const version = [
-  {
-    name: 'Windows Frpc',
-    arch: 'amd64',
-    url: 'https://io.ivampiresp.com/Software/MEFrpRelease/MirrorEdgeFrp_0.46.0_beta_windows_amd64.zip',
-  },
-  {
-    name: 'Windows Python 图形化启动器',
-    arch: 'amd64',
-    url: 'https://io.ivampiresp.com/Software/MEFrpRelease/Mirror_Edge_Frp_Python_Win.zip',
-  },
-  {
-    name: 'Windows C# .NET 图形化启动器',
-    arch: 'amd64',
-    url: 'http://124.223.35.239/download/mefrp/',
-  },
-  {
-    name: 'Linux Frpc',
-    arch: 'amd64',
-    url: 'https://io.ivampiresp.com/Software/MEFrpRelease/MirrorEdgeFrp_0.46.0_beta_linux_amd64.tar.gz',
-  },
-  {
-    name: '更多版本 Frpc',
-    arch: 'any',
-    url: 'https://io.ivampiresp.com/Software/MEFrpRelease',
-  },
-]
+  const version = [
+    {
+      name: 'Windows Frpc',
+      arch: 'amd64',
+      url: 'https://r2.laecloud.com/MEFrpRelease/MirrorEdgeFrp_0.46.1_beta_windows_amd64.zip',
+    },
+    {
+      name: 'Windows Frpc i386',
+      arch: 'i386',
+      url: 'https://r2.laecloud.com/MEFrpRelease/MirrorEdgeFrp_0.46.1_beta_windows_386.zip',
+    },
+    {
+      name: 'Windows Python 图形化启动器',
+      arch: 'amd64',
+      url: 'https://r2.laecloud.com/MEFrpRelease/Mirror_Edge_Frp_Python_Win.zip',
+    },
+    {
+      name: 'Windows C# .NET 图形化启动器',
+      arch: 'amd64',
+      url: 'http://124.223.35.239/download/mefrp/',
+    },
+    {
+      name: 'Linux Frpc amd64',
+      arch: 'amd64',
+      url: 'https://r2.laecloud.com/MEFrpRelease/frp_MirrorEdgeFrp_0.46.1_beta_linux_amd64.tar.gz',
+    },
+    {
+      name: 'Linux Frpc arm64',
+      arch: 'arm64',
+      url: 'https://r2.laecloud.com/MEFrpRelease/frp_MirrorEdgeFrp_0.46.1_beta_linux_arm64.tar.gz',
+    },
+    {
+      name: 'Darwin Frpc amd64',
+      arch: 'amd64',
+      url: 'https://r2.laecloud.com/MEFrpRelease/frp_MirrorEdgeFrp_0.46.1_beta_darwin_amd64.tar.gz',
+    },
+    {
+      name: 'Darwin Frpc arm64',
+      arch: 'arm64',
+      url: 'https://r2.laecloud.com/MEFrpRelease/frp_MirrorEdgeFrp_0.46.1_beta_darwin_arm64.tar.gz',
+    },
+  ]
 
-const items = ref(version)
+  const items = ref(version)
 
-const render = (value) => {
-  // console.log(value, typeof (value))
-  if (value && value.startsWith('http'))
-    return h(NA, {href: value, target: '_blank'}, () => {
-      return '下载'
-    })
-  else return value
-}
+  const render = (value) => {
+    // console.log(value, typeof (value))
+    if (value && value.startsWith('http'))
+      return h(NA, { href: value, target: '_blank' }, () => {
+        return '下载'
+      })
+    else return value
+  }
 
-const filter_options = [
-  {
-    label: 'amd64',
-    value: 'amd64',
-  },
-  {
-    label: 'any',
-    value: 'any',
-  },
-]
+  const filter_options = [
+    {
+      label: 'amd64',
+      value: 'amd64',
+    },
+    {
+      label: 'any',
+      value: 'any',
+    },
+  ]
 
-// const archFilter = ref(['amd64':'amd64'])
+  // const archFilter = ref(['amd64':'amd64'])
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
